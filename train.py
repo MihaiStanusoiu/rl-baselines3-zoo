6529e12a6cac216d53b334db6b194ef4022202ad
Mihai Stanusoiu <e12118533@student.tuwien.ac.at>
2024-08-08 22:14:40 +0200

    Added CfCPPO hyperparameters

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 from rl_zoo3.train import train
+from cfcppo.src.policies import CfcActorCriticPolicy
 
 if __name__ == "__main__":
+    # register_policy("CfcActorCriticPolicy", CfcActorCriticPolicy)
     train()
